b61294a31201889204c456af196fe9735fc4a40b
ABardot <aabardot5@gmail.com>
2018-01-29 11:41:53 -0500

    Added the Eat method

--- IntroToOop/Cat.cs
+++ IntroToOop/Cat.cs
@@ -7,6 +7,16 @@ namespace IntroToOop
         //States
 
         private string name; //Fields
+        private int age;
+        private string furColor;
+        private bool isHungry;
+
+        public string Name
+        {
+            get {return this.Name}
+            set { this.Name = value}
+        }
+           
 
         //Constructors where the class behaves
         public Cat() //Default Constructor no parameter are always need when creating a new constructor 
@@ -14,7 +24,23 @@ namespace IntroToOop
 
         }
 
+        public Cat(string name, int age, string furColor)
+        {
+            this.name = name;
+            this.age = age;
+            this.furColor = furColor;
 
+        }
+
+        public void Eat() //Method
+        {
+            if(isHungry)
+            {
+                isHungry = false;
+            }
+
+            Console.WriteLine( "Is the cat hungry? " + isHungry);
+        }
 
     }
 }
