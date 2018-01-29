5075dde69b0ffa5a6c29a9db53247d900aa82fc3
ABardot <aabardot5@gmail.com>
2018-01-29 11:11:30 -0500

    Created my first class and constructor

--- IntroToOop/Cat.cs
+++ IntroToOop/Cat.cs
@@ -4,5 +4,17 @@ namespace IntroToOop
 {
     class Cat
     {
+        //States
+
+        private string name; //Fields
+
+        //Constructors where the class behaves
+        public Cat() //Default Constructor no parameter are always need when creating a new constructor 
+        {
+
+        }
+
+
+
     }
 }
